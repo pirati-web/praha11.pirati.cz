b26acfdf7a9a49fb007f42e3420f1579ad6088f1
Jan Suchánek <jan.suchanek@gmail.com>
2022-08-25 23:07:49 +0200

    Update reseni.js

--- assets/js/reseni.js
+++ assets/js/reseni.js
@@ -38,6 +38,37 @@ $(function(){
 	
 	
 	
+	//Show/hide solutions
+	$('div.reseni2').wrap('<div class="solution_wrapper"></div>');
+	if (!_allSolutionVisible)
+	{
+		$('div.reseni2').hide();
+		$('div.solution_wrapper').prepend('<div class="control show"><button class="showhide">' + _showText + '</button><br><br><a class="showhideall">' + _showAllText + '</a></div>');
+	}
+	else
+	{
+		$('div.solution_wrapper').prepend('<div class="control hide"><button class="showhide">' + _hideText + '</button><br><br><a class="showhideall">' + _hideAllText + '</a></div>');
+	}
+	//show hide one
+	$('button.showhide').click(function() {
+		if ($(this).parent().parent().children('div.reseni2').is(':hidden'))
+		{
+			$(this).parent().parent().children('div.reseni2').slideDown('normal;');
+			$(this).text(_hideText);
+
+		}
+		else
+		{
+			$(this).parent().parent().children('div.reseni2').slideUp('normal;');
+			$(this).text(_showText);
+
+		}
+	});
+	
+	
+	
+	
+	
 	
 	//show hide all
 	$('button.showhideall').click(function() {
